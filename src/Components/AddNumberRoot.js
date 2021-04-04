02f4f25bf67d1962e3b12af21ad99e77eac9f4f2
bambwc20 <bambwc20@naver.com>
2021-04-04 20:16:24 +0900

    1.리덕스없이 리액트로만 구현한 여러컴포넌트가 관계를 이루고 있는 애플리케이션

---
+++ src/Components/AddNumberRoot.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import AddNumber from "./AddNumber";
+
+class AddNumberRoot extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Number Root</h1>
+        <AddNumber
+          onClick={(size) => {
+            this.props.onClick(size);
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default AddNumberRoot;
